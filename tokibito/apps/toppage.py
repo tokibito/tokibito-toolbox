ddae1c196041c2f26365002e6cfb13846b5b7e94
Shinya Okano <tokibito@gmail.com>
2010-12-07 03:45:04 +0900

    modify toppage

--- tokibito/apps/toppage.py
+++ tokibito/apps/toppage.py
@@ -2,6 +2,7 @@ from google.appengine.ext import webapp
 
 class TopPageHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('toppage.')
+        from google.appengine.ext.webapp import template
+        self.response.out.write(template.render('templates/index.html', {}))
 
 application = webapp.WSGIApplication([('/', TopPageHandler)])
