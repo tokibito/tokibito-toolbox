5dbda271f65e4f68e286317f90a7e71a98703d03
Shinya Okano <tokibito@gmail.com>
2011-03-31 04:31:38 +0900

    smartphone対応

--- tokibito/apps/toppage.py
+++ tokibito/apps/toppage.py
@@ -1,6 +1,10 @@
+import re
+
 from google.appengine.ext import webapp
 
 from tokky.generics import TemplatePageHandler
+
+RE_UA_SMARTPHONE = re.compile(r'(Android|iPhone)')
 
 class TopPageHandler(TemplatePageHandler):
     template_name = 'templates/index.html'
@@ -8,6 +12,7 @@ class TopPageHandler(TemplatePageHandler):
     def get_context(self, *args, **kwargs):
         from apps.presentation import api as presentation_api
         return {
+            'is_smartphone': bool(RE_UA_SMARTPHONE.search(self.request.user_agent)),
             'presentation_objects': presentation_api.get_presentation_all()
         }
 
